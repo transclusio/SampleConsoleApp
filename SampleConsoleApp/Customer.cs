01f34a045680fa6dca22daeeec22cc55a7ebb964
costasp <costasp@COSTASP1>
2017-01-18 23:20:26 +0200

    Add project files.

---
+++ SampleConsoleApp/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Customer
+    {
+        public String FName { get; set; }
+        public String Lname { get; set; }
+        public double Salary { get; set; }
+
+        public Customer()
+        {
+            this.FName = "";
+            this.Lname = "";
+            this.Salary = 0;
+        }
+
+        public Customer(string fname, string lname, double salary)
+        {
+            this.FName = fname;
+            this.Lname = lname;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Hi from {0}, {1}", this.FName, this.Lname);
+
+        }
+    }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer("Costas", "Petrou", 1000);
+            Console.WriteLine("Welcome to this app! Press any key to close");
+            Console.WriteLine(c.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
